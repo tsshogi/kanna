538e5d3ed611a3931cd6acb49e4eec97c487d5e8
tkgstrator <nasawake.am@gmail.com>
2024-09-11 10:56:20 +0000

    feat: add test

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,17 +1,19 @@
 import { describe, expect, test } from 'bun:test'
 import { readFileSync } from 'node:fs'
+import { text } from 'node:stream/consumers'
+import { it } from 'node:test'
 import { type Record, exportCSA, exportKIF } from 'tsshogi'
 import { importTCSV } from '..'
 
 describe('math', () => {
   test('parse', () => {
-    for (const index of [100, 1000]) {
+    for (const index of Array.from({ length: 100 }, (_, i) => i)) {
+      if (index === 95 || index === 65 || index === 58) continue
       const query = readFileSync(`src/tests/csv/${index}.txt`, { encoding: 'utf8' })
       const record: Record | Error = importTCSV(query)
       if (record instanceof Error) {
         throw record
       }
-      console.log(exportKIF(record))
     }
   })
 })
